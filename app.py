5b91b70669437b7a15ad0ad458ef5759b138fa11
DaniHere <=>
2025-03-02 02:31:25 +0200

    Revision v2.2 (Feb 2025)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -14,14 +14,8 @@ def achievements():
 def hobbies():
     return render_template('hobbies.html')
 
-@app.route('/contact', methods=['GET', 'POST'])
+@app.route('/contact')
 def contact():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        message = request.form['message']
-        # Add functionality to send the message (e.g., via email)
-        return render_template('contact.html', success=True)
     return render_template('contact.html')
 
 if __name__ == '__main__':
